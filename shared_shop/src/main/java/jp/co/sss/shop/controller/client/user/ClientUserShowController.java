e89043dab4ac468e106514701ca9a4e46b98cdf8
nanakat <nanakat@C010423405.zipangu.cac.co.jp>
2026-06-08 16:33:13 +0900

    add: 会員詳細表示のコントローラを追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserShowController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserShowController.java
@@ -1,1 +1,43 @@
-//test
+package jp.co.sss.shop.controller.client.user;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+import jp.co.sss.shop.bean.UserBean;
+import jp.co.sss.shop.entity.User;
+import jp.co.sss.shop.repository.UserRepository;
+import jp.co.sss.shop.util.Constant;
+
+@Controller
+public class ClientUserShowController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    HttpSession session;
+
+    @RequestMapping(path = "/client/user/detail")
+    public String showUser(Model model) {
+
+        UserBean loginUser = (UserBean) session.getAttribute("user");
+
+        User user = userRepository.findByIdAndDeleteFlag(
+                loginUser.getId(), Constant.NOT_DELETED);
+
+        if (user == null) {
+            return "redirect:/syserror";
+        }
+
+        UserBean userBean = new UserBean();
+        BeanUtils.copyProperties(user, userBean);
+
+        model.addAttribute("userBean", userBean);
+
+        return "client/user/detail";
+    }
+}
