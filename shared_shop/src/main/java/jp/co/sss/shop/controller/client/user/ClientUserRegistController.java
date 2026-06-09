e39ceae94adec0f3c78dd386a7b6b1e18dba63f7
knamba <knamba@C010423464.zipangu.cac.co.jp>
2026-06-09 11:21:22 +0900

    新規登録ボタンメソッドの変更

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
@@ -2,11 +2,12 @@ package jp.co.sss.shop.controller.client.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.http.HttpSession;
-import jp.co.sss.shop.bean.UserBean;
 import jp.co.sss.shop.form.UserForm;
 import jp.co.sss.shop.repository.UserRepository;
 
@@ -15,30 +16,73 @@ public class ClientUserRegistController {
 	@Autowired
 	UserRepository userrepository;
 
+	@Autowired
+	HttpSession session;
+
+	//新規登録リンク クリック時処理
 	@RequestMapping(path = "/regist/input/init", method = RequestMethod.GET)
-	public String userRegistInput(HttpSession session) {
+	public String RegistInputInit() {
 
 		//入力フォーム情報を新規生成
 		UserForm userForm = new UserForm();
+
 		//入力フォーム情報をセッションスコープに保存
 		session.setAttribute("user", userForm);
 
-		return "redirect://resist/input";
+		return "redirect:/resist/input";
 	}
 
+	//新規登録ボタン 押下時処理、確認画面-戻るボタン 押下時処理
 	@RequestMapping(path = "/regist/input", method = RequestMethod.POST)
-	public String userRegistInputGET(HttpSession session) {
+	public String userRegistInputPOST(HttpSession session) {
 
 		//セッションスコープより入力情報を取り出す
-		UserForm userForm = (UserForm) session.getAttribute("userForm");
-		if (userForm == null) {
-			userForm = new UserForm();
-			userForm.setAuthority(((UserBean) session.getAttribute("user")).getAuthority());
+		UserForm userForm = (UserForm) session.getAttribute("user");
 
-			//空の入力フォーム情報をセッションに保持 登録ボタンからの遷移
+		if (userForm == null) {
+
+			//入力フォーム情報を新規生成
+			userForm = new UserForm();
+
+			//必要な情報を入力フォーム情報にセット
+			userForm.setId(userForm.getId());
+			userForm.setEmail(userForm.getEmail());
+			userForm.setName(userForm.getName());
+			userForm.setPassword(userForm.getPassword());
+			userForm.setPostalCode(userForm.getPostalCode());
+			userForm.setAddress(userForm.getAddress());
+			userForm.setPhoneNumber(userForm.getPhoneNumber());
+
+			//入力フォーム情報をセッションスコープに保存
 			session.setAttribute("userForm", userForm);
+
 		}
 		return "redirect:/regist/input";
+	}
+
+	//登録画面表示処理
+	@RequestMapping(path = "/regist/input", method = RequestMethod.GET)
+	public String userRegistInputGET(Model model) {
+
+		//セッションスコープから入力フォーム情報を取得
+		UserForm userForm = (UserForm) session.getAttribute("userForm");
+
+		//入力エラー確認
+		BindingResult result = (BindingResult) session.getAttribute("result");
+
+		if (result != null) {
+
+			//セッションにエラー情報がある場合、エラー情報をリクエストスコープに設定
+			model.addAttribute("org.springframework.validation.BindingResult.userForm", result);
+
+			//セッションからエラー情報を削除
+			session.removeAttribute("result");
+		}
+
+		// 入力フォーム情報をリクエストスコープに設定
+		model.addAttribute("userForm", userForm);
+
+		return "/regist_input";
 	}
 
 }
