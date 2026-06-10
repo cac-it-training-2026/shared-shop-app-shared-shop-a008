d2e54d4c7c831ad7976441c8a1776353b5a240f4
megumimo <megumimo@C010423663.zipangu.cac.co.jp>
2026-06-10 16:57:36 +0900

    add: addressInputメソッドの処理追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import jp.co.sss.shop.bean.UserBean;
+import jp.co.sss.shop.entity.User;
 import jp.co.sss.shop.form.OrderForm;
 import jp.co.sss.shop.repository.OrderRepository;
+import jp.co.sss.shop.repository.UserRepository;
 
 /**
  * 注文登録の基本クラス
@@ -21,26 +24,66 @@ import jp.co.sss.shop.repository.OrderRepository;
 @Controller
 public class ClientOrderRegistController {
 
-	/** リポジトリのオブジェクトを生成 */
+	/** Orderリポジトリのオブジェクトを生成 */
 	@Autowired
-	OrderRepository repository;
+	OrderRepository orderRepository;
+
+	/** Userリポジトリのオブジェクトを生成 */
+	@Autowired
+	UserRepository userRepository;
 
 	/**
 	 * @param session
-	 * @redirect 
+	 * @redirect "client/order/address/input" 届け先入力画面
 	 */
 	@RequestMapping(path = "/client/order/address/input", method = RequestMethod.POST)
 	public String addressInputPost(HttpSession session) {
+
+		// ログイン会員情報をセッションから取得
+		UserBean loginUser = (UserBean) session.getAttribute("user");
+
+		// 取得したログイン会員情報のユーザIDを条件にDBからユーザ情報を取得
+		User user = userRepository.getReferenceById(loginUser.getId());
+
+		// 注文入力フォームを生成
+		OrderForm orderForm = new OrderForm();
+
+		// 取得ユーザー情報をフォームにコピー
+		orderForm.setPostalCode(user.getPostalCode());
+		orderForm.setAddress(user.getAddress());
+		orderForm.setName(user.getName());
+		orderForm.setPhoneNumber(user.getPhoneNumber());
+
+		// 支払方法の初期値としてクレジットカードを設定
+		orderForm.setPayMethod(1);
+
+		// セッションに保存
+		session.setAttribute("orderForm", orderForm);
+
+		// 届け先入力画面表示処理へリダイレクト
 		return "redirect:/client/order/address/input";
 	}
 
 	/**
-	 * @param sessino
+	 * 届け先入力画面を表示するメソッド
+	 * 
+	 * @param session
 	 * @param model
-	 * @return
+	 * @return "/client/order/address_input.html" 届け先入力画面
 	 */
 	@RequestMapping("/client/order/address/input")
-	public String addressInputGet(HttpSession sessino, Model model) {
+	public String addressInputGet(HttpSession session, Model model) {
+		// セッションスコープから注文入力フォーム情報を取得
+		OrderForm orderForm = (OrderForm) session.getAttribute("orderForm");
+		// 注文入力フォーム情報をリクエストスコープに設定
+		model.addAttribute("orderForm", orderForm);
+
+		// memo
+		//		・セッションスコープに入力エラー情報がある場合
+		//		　- 取得したエラー情報をリクエストスコープに設定
+		//		　- セッションスコープから、エラー情報を削除
+
+		// 登録画面表示
 		return "/client/order/address_input";
 	}
 
