ed47e7beacb2dd547bae12ff58e5b8e9f8ecc853
megumimo <megumimo@C010423663.zipangu.cac.co.jp>
2026-06-11 09:55:15 +0900

    add: 届け先入力機能追加、支払い方法選択画面表示追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
@@ -49,6 +49,7 @@ public class ClientOrderRegistController {
 		OrderForm orderForm = new OrderForm();
 
 		// 取得ユーザー情報をフォームにコピー
+		orderForm.setId(user.getId());
 		orderForm.setPostalCode(user.getPostalCode());
 		orderForm.setAddress(user.getAddress());
 		orderForm.setName(user.getName());
@@ -69,7 +70,7 @@ public class ClientOrderRegistController {
 	 * 
 	 * @param session
 	 * @param model
-	 * @return "/client/order/address_input.html" 届け先入力画面
+	 * @return "client/order/address_input.html" 届け先入力画面
 	 */
 	@RequestMapping("/client/order/address/input")
 	public String addressInputGet(HttpSession session, Model model) {
@@ -78,10 +79,16 @@ public class ClientOrderRegistController {
 		// 注文入力フォーム情報をリクエストスコープに設定
 		model.addAttribute("orderForm", orderForm);
 
-		// memo
-		//		・セッションスコープに入力エラー情報がある場合
-		//		　- 取得したエラー情報をリクエストスコープに設定
-		//		　- セッションスコープから、エラー情報を削除
+		// エラー情報をセッションから取得
+		BindingResult result = (BindingResult) session.getAttribute("errors");
+
+		// セッションスコープに入力エラー情報がある場合
+		if (result != null) {
+			// 取得したエラー情報をリクエストスコープに設定
+			model.addAttribute("org.springframework.validation.BindingResult.orderForm", result);
+			// セッションスコープから、エラー情報を削除
+			session.removeAttribute("errors");
+		}
 
 		// 登録画面表示
 		return "/client/order/address_input";
@@ -91,11 +98,23 @@ public class ClientOrderRegistController {
 	 * @param orderForm
 	 * @param result
 	 * @param session
-	 * @redirect
+	 * @redirect "client/order/payment/input"
 	 */
 	@RequestMapping(path = "/client/order/payment/input", method = RequestMethod.POST)
 	public String paymentInputPost(@Valid @ModelAttribute OrderForm orderForm, BindingResult result,
 			HttpSession session) {
+
+		// セッションスコープから注文入力フォーム情報を取得
+		orderForm = (OrderForm) session.getAttribute("orderForm");
+		// BindingResultオブジェクトに入力エラー情報がある場合
+		if (result.hasErrors()) {
+			// 入力エラー情報をセッションスコープに設定
+			session.setAttribute("errors", result);
+			// 届け先入力画面表示処理にリダイレクト
+			return "redirect:/client/order/address/input";
+		}
+
+		// 支払方法選択画面表示処理にリダイレクト
 		return "redirect:/client/order/payment/input";
 	}
 
@@ -106,14 +125,20 @@ public class ClientOrderRegistController {
 	 */
 	@RequestMapping("/client/order/payment/input")
 	public String paymentInputGet(HttpSession session, Model model) {
+		//		・セッションスコープから注文入力フォーム情報を取得
+		OrderForm orderForm = (OrderForm) session.getAttribute("orderForm");
+		//		・注文フォーム情報をリクエストスコープに設定
+		model.addAttribute("payMethod", orderForm.getPayMethod());
+		//		・支払方法選択画面表示
 		return "/client/order/payment_input";
 	}
 
 	/**
-	 * @redirect
+	 * @redirect "client/order/address/input" 支払方法選択画面
 	 */
 	@RequestMapping(path = "/client/order/payment/back", method = RequestMethod.POST)
 	public String paymentBack() {
+		// 支払い方法選択画面 表示処理へリダイレクト
 		return "redirect:/client/order/address/input";
 	}
 
@@ -124,6 +149,10 @@ public class ClientOrderRegistController {
 	 */
 	@RequestMapping(path = "/client/order/check", method = RequestMethod.POST)
 	public String orderCheckPost(HttpSession session, Model model) {
+		//		・セッションスコープから注文入力フォーム情報を取得
+		//		・画面から入力された支払方法を取得した注文入力フォーム情報に設定
+		//		・注文入力フォーム情報をセッションスコープに保存
+		//		・注文確認画面表示処理へリダイレクト
 		return "redirect:/client/order/check";
 	}
 
@@ -134,6 +163,20 @@ public class ClientOrderRegistController {
 	 */
 	@RequestMapping("/client/order/check")
 	public String orderCheckGet(HttpSession session, Model model) {
+		//		・セッションスコープから注文情報を取得
+		//		・セッションスコープから買い物かご情報を取得
+		//		・注文商品の最新情報をDBから取得し、商品の在庫チェックを行う
+		//		・在庫不足、在庫切れ商品がある場合
+		//		- 注文警告メッセージをリクエストスコープに保存
+		//		- 在庫数にあわせて、買い物かご情報を更新（注文数、在庫数）
+		//		- 在庫切れの商品は、買い物かごから削除
+		//		・在庫状況を反映した買い物かご情報をセッションに保存
+		//		・買い物かご情報から、商品ごとの金額小計を算出し、注文商品情報リストに保存
+		//		・注文商品情報リストから合計金額を算出する
+		//		・合計金額をリクエストスコープに設定
+		//		・注文商品情報リストをリクエストスコープに設定
+		//		・注文入力フォーム情報をリクエストスコープに設定
+		//		・注文確認画面表示
 		return "/client/order/check";
 	}
 
@@ -143,6 +186,17 @@ public class ClientOrderRegistController {
 	 */
 	@RequestMapping(path = "/client/order/complete", method = RequestMethod.POST)
 	public String orderCompletePost(HttpSession session) {
+		//		・セッションスコープから注文情報を取得
+		//		・セッションスコープから買い物かご情報を取得
+		//		・注文商品の在庫チェックをする
+		//		・在庫切れまたは在庫不足の商品がある場合
+		//		-注文確認画面表示処理へリダイレクト
+		//		リダイレクト:” /client/order/check” 
+		//		・注文情報情報を元にDB登録用エンティティオブジェクトを生成
+		//		・注文テーブルおよび注文商品テーブルのDB登録実施
+		//		・セッションスコープの注文入力フォーム情報と買い物かご情報を削除
+		//		・注文完了画面表示処理にリダイレクト
+
 		return "redirect:/client/order/complete";
 	}
 
@@ -152,6 +206,7 @@ public class ClientOrderRegistController {
 	 */
 	@RequestMapping("/client/order/complete")
 	public String orderCompleteGet(Model model) {
+		//		・注文完了画面表示
 		return "/client/order/complete";
 	}
 }
